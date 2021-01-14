b40138292626a4bdd84b9d0b7226708a48290971
Hannah Malm <hannahemalm616@gmail.com>
2021-01-14 01:31:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,22 @@
 class Dog
-	def initialize(name,breed)
+	def initialize(name)
 		@name = name
-		@breed = breed
 	end 
-	end 
+
+	
+# 	def initialize(name,breed)
+# 		@name = name
+# 		@breed = breed
+# 	end 
+# 	end 
 	
 	def name=(name)
 	  @name = name
   end 
   
-  def breed = (breed)
-    @breed = breed
-  end 
+  # def breed = (breed)
+  #   @breed = breed
+  # end 
 end 
   
   
